70b613eb03d12ea3398146c2c541c0937631929a
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2022-10-30 16:36:58 +0100

    builtin led flash

--- src/main.cc
+++ src/main.cc
@@ -8,9 +8,27 @@
 static struct fl_driver driver;
 static struct wave* wv;
 
-#include "utils/pool.h"
+#define BUILTIN_TICK_INTERVAL 500
 
-static pool_byte_ty pool[POOL_REAL_SIZE(10, sizeof(int))];
+/**
+ * @brief Function to indicate whether or not MCU is running
+ *
+ */
+static void
+_builtin_tick(void)
+{
+    static uint64_t time = 0;
+    static uint8_t mode;
+
+    uint64_t mill = millis();
+
+    if (mill - time > BUILTIN_TICK_INTERVAL) {
+        mode = !mode;
+        time = mill;
+
+        digitalWrite(LED_BUILTIN, mode);
+    }
+}
 
 void
 setup()
@@ -18,23 +36,14 @@ setup()
     pinMode(LED_BUILTIN, OUTPUT);
     randomSeed(analogRead(A0));
     Serial.begin(9600);
+
     driver = driver_init(60 * 2);
-
-    Serial.print("Initializing pool with size: ");
-    Serial.println(POOL_REAL_SIZE(10, sizeof(int)));
-    p_init(pool, 10, sizeof(int));
-
     wv = wave_random(&driver);
-#if 0
-#else
-    // wv = NULL;
-#endif
 }
 
 void
 loop()
 {
-#if 1
     if (wv && !wave_tick(wv)) {
         driver.fastled->clear(true);
         wave_destroy(wv);
@@ -42,18 +51,5 @@ loop()
         Serial.println((int)(uintptr_t)wv);
     };
 
-    Serial.println("Alloc: ");
-    void* mem = p_alloc(pool);
-    Serial.println("Free: ");
-    p_free(mem);
-    Serial.println("Done");
-    digitalWrite(LED_BUILTIN, HIGH);
-
-#elif 0
-    driver.fastled->clear();
-    driver.out[0] = CRGB::Blue;
-    driver.fastled->show();
-#else
-    digitalWrite(LED_BUILTIN, HIGH);
-#endif
+    _builtin_tick();
 }
